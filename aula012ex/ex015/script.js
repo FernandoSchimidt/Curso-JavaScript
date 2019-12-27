3e2a1f67a41e940e1fd7b0287c99adf27d2f78f7
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2019-12-26 23:07:01 -0300

    aula

--- aula012ex/ex015/script.js
+++ aula012ex/ex015/script.js
@@ -5,11 +5,38 @@ function verificar(){
     var fAno = document.getElementById('txtano')
     var res = document.getElementById('res')
 
-    if(fAno.value.length == 0 || fAno.value > ano){
+    if(fAno.value.length == 0 ||Number (fAno.value) > ano){
         window.alert('[ERRO] Varifique os dados e tente novamente!')
     }else{
          var fsex = document.getElementsByName ('radsex')
          var idade = ano  - Number(fano.value)
-         res.innerHTML`Idade Calculada: ${idade}`
-    }
+        var genero = ''
+        var img  = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+
+            }else if (idade <= 21){
+
+            }else if(idade <= 50){
+
+            }else{
+
+            }
+        }else if(fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+
+            }else if (idade <= 21){
+
+            }else if(idade <= 50){
+
+            }else{
+                
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    } 
 }
